519f642bb373e6335fdeeb6719d1e0966c1281fd
Your Name <you@example.com>
2026-07-20 20:02:28 -0300

    render

--- almoxarifado_mf/settings.py
+++ almoxarifado_mf/settings.py
@@ -78,15 +78,22 @@ WSGI_APPLICATION = 'almoxarifado_mf.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.postgresql',
-         'NAME': os.environ.get('DB_NAME'),
-         'USER': os.environ.get('DB_USER'),
-         'PASSWORD': os.environ.get('DB_PASSWORD'),
-         'HOST': os.environ.get('DB_HOST'),
-         'PORT': os.environ.get('DB_PORT'),
-     }
- }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
+}
+#DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.environ.get('DB_NAME'),
+#         'USER': os.environ.get('DB_USER'),
+#         'PASSWORD': os.environ.get('DB_PASSWORD'),
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': os.environ.get('DB_PORT'),
+#     }
+# }
 
 
 # Password validation
